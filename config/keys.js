6bf47ed7d866bdfe32a0a93a520390b408c6542b
Alfie Darko <mrhazzy@gmail.com>
2017-09-15 17:07:51 +0100

    fixed bug

--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,7 @@
 
 if (process.env.NODE_ENV === "production") {
 	// we are in production - return production keys
-	module.exports = req("./prod");
+	module.exports = require("./prod");
 } else {
 	// we are in development - return dev keys
 	module.exports = require("./dev");
